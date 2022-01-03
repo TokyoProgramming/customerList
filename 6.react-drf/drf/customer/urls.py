bf85ef7e328e29fbccedcc6eaf9b02f417fde9be
TokyoProgramming <samuraidjango.jp@gmail.com>
2022-01-03 20:13:59 +0900

    delete

--- 6.react-drf/drf/customer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('', views.customerList, name='customers'),
-    path('create', views.customerCreate, name='create'),
-    path('update/<str:pk>', views.customerUpdate, name='update'),
-    path('delete/<str:pk>', views.customerDelete, name='delete')
-]
